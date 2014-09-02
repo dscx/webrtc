f6fd627c55f0acfdc98665c8384bd7aa4e86ddf2
Gabs00 <gewen87@gmail.com>
2014-09-01 20:45:22 -0400

    implimented signaling

--- server/config/socketio.js
+++ server/config/socketio.js
@@ -4,54 +4,112 @@
 
 'use strict';
 
+
+
 var config = require('./environment');
+var db = require('../api/rooms/rooms.controller.js');
+
+var allRooms = {};
+var clients = [];
+
 
 // When the user disconnects.. perform this
 function onDisconnect(socket) {
 }
 
+function closeRoom(room){
+  db.end(room);
+}
+
+
 // When the user connects.. perform this
 function onConnect(socket) {
   // When the client emits 'info', this listens and executes
   socket.on('info', function (data) {
-    console.info('[%s] %s', socket.address, JSON.stringify(data, null, 2));
+    console.log('testing')
+    console.info('[%s] %s', socket.address, JSON.stringify(data, null, 2), 'PIES');
   });
 
   // Insert sockets below
   require('../api/thing/thing.socket').register(socket);
 }
 
-module.exports = function (socketio) {
-  // socket.io (v1.x.x) is powered by debug.
-  // In order to see all the debug output, set DEBUG (in server/config/local.env.js) to including the desired scope.
-  //
-  // ex: DEBUG: "http*,socket.io:socket"
 
-  // We can authenticate socket.io users and access their token through socket.handshake.decoded_token
-  //
-  // 1. You will need to send the token in `client/components/socket/socket.service.js`
-  //
-  // 2. Require authentication here:
-  // socketio.use(require('socketio-jwt').authorize({
-  //   secret: config.secrets.session,
-  //   handshake: true
-  // }));
+module.exports = function (socketio, cache) {
+ 
+var namespace = socketio.of('/rooms');
+
+ namespace.on('connection', function(socket){
+    console.log('someone connected to the nsp');
+    socket.address = socket.handshake.address !== null ?
+          socket.handshake.address.address + ':' + socket.handshake.address.port :
+          process.env.DOMAIN;
+    
+    socket.on('room', function(roomId){
+      socket.join(roomId.room);
+      if(!allRooms[roomId.room]){
+        allRooms[roomId.room] = [];
+      }
+      
+      var pid = allRooms[roomId.room].length;
+      socket.webRoom = roomId.room;
+      allRooms[roomId.room].push(socket);
+      //console.log(socket.webRoom, 'room name');
+      console.log(allRooms[socket.webRoom].length); 
+
+           
+      var room = roomId.room;
+
+      socket.on('disconnect', function(socket){
+        if(room !== undefined){
+          allRooms[room].splice(pid, 1);
+            if(allRooms[room].length === 0){
+              delete allRooms[room];
+              var roomHash = cache[room];
+              delete cache[room];
+              closeRoom(roomHash);
+           }
+        }
+      });
+
+      socket.on('offer', function(offer){
+       socket.broadcast.emit('offer', {'offer': offer, 'pid': pid, 'room': room});
+        // console.log(socket.address, 'address');
+        console.info('starting call...');
+      });
+
+      socket.on('answer', function(response){
+        var target = allRooms[response.room][response.pid];
+        target.emit('answer', response.answer);
+        console.info('answering call...');
+      });
+
+    });
+
+  })
+
 
   socketio.on('connection', function (socket) {
+
     socket.address = socket.handshake.address !== null ?
             socket.handshake.address.address + ':' + socket.handshake.address.port :
             process.env.DOMAIN;
 
+
     socket.connectedAt = new Date();
 
-    // Call onDisconnect.
-    socket.on('disconnect', function () {
-      onDisconnect(socket);
-      console.info('[%s] DISCONNECTED', socket.address);
-    });
-
     // Call onConnect.
-    onConnect(socket);
+    //onConnect(socket);
     console.info('[%s] CONNECTED', socket.address);
+
+    
+
+    // Call onDisconnect.
+    // socket.on('disconnect', function () {
+    //   //onDisconnect(socket);
+    //   console.info('[%s] DISCONNECTED', socket.address);
+    // });
   });
+
+
 };
---
+++ server/routespec.js
@@ -0,0 +1,18 @@
+var routes = require('/routes.js');
+
+//////////
+
+describe('', function(){
+
+  beforeEach(function(){
+    //clear routes
+    var rooms = {};
+  });
+
+  describe('create', function(){
+    it('should create a new room', function(){
+      
+    })
+  })
+
+})
