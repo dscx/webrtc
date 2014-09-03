359f66e5f16984f89fd9b364bec63644d8925672
Dan <dnc223@gmail.com>
2014-09-03 07:19:22 -0400

    On homepage, change styling of navbar, add input field

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -3,8 +3,6 @@
 angular.module('webrtcApp')
   .controller('NavbarCtrl', function ($scope, $location, Auth) {
     $scope.menu = [{
-      'title': 'Home',
-      'link': '/'
     }];
 
     $scope.isCollapsed = true;
