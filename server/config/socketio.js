1d8aadf3551d1077acecd57348a9936c16b75c71
Dan Chamberlain <dscx@dan.local>
2014-09-04 13:56:52 -0700

    fixed PID bug

--- server/config/socketio.js
+++ server/config/socketio.js
@@ -8,7 +8,6 @@
 
 var config = require('./environment');
 var db = require('../api/rooms/rooms.controller.js');
-
 var allRooms = {};
 
 // When the user disconnects.. perform this
@@ -52,13 +51,19 @@ var namespace = socketio.of('/rooms');
         allRooms[roomId.room] = [];
       }
       
-      var pid = allRooms[roomId.room].length;
       allRooms[roomId.room].push(socket);
+      var pid = allRooms[roomId.room].length;
 
       var room = roomId.room;
       var otherPids = Array.apply(null, {length: pid}).map(Number.call, Number);
-      console.log(otherPids.slice(-1), "the rest");
-      socket.emit('confirm', {pids:otherPids, pid:pid, room:room}); //include PID's
+     console.log(otherPids, "PIDS 1");
+      if(otherPids.length > 1){
+        otherPids = otherPids.splice(0, otherPids.length - 1);
+      } else {
+        otherPids = [];
+      }
+      console.log(otherPids, "PIDS");
+      socket.emit('confirm', {pids:otherPids, pid:pid, room:room});
 
       //emits to all on conference the new participants information
       socket.to(room).emit('new', {pid:pid});
@@ -67,7 +72,7 @@ var namespace = socketio.of('/rooms');
           //allRooms[room][pid] = null;
           allRooms[room].splice(pid, 1);
           namespace.to(room).emit('left', {pid:pid});
-          console.log(allRooms[room].length, "LENGTH");
+          console.log(allRooms[room].length, "remaining participants");
             if(allRooms[room].length === 0){
               delete allRooms[room];
               var roomHash = cache[room];
